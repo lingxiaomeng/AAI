e932e1af3173c8fb9eda61fe6beba1ba1b8d624e
LingxiaoMeng <574243996@qq.com>
2021-12-13 21:49:09 +0800

    add IPGA

---
+++ GA-for-mTSP/IPGA/main.py
@@ -0,0 +1,52 @@
+from numpy.core.shape_base import block
+from galogic import *
+import matplotlib.pyplot as plt
+import progressbar
+import numpy as np
+import counter
+from dustbin import Dustbin
+from globals import seedValue, populationSize, numGenerations, city
+
+pbar = progressbar.ProgressBar()
+
+# Add Dustbins
+for i in range(numNodes):
+    RouteManager.addDustbin(Dustbin(city[i][1], city[i][2], i))
+RouteManager.calculateDistanceMatrix()
+
+
+random.seed(seedValue)
+yaxis = []  # Fittest value (distance)
+xaxis = []  # Generation count
+counter.count = 0
+
+pop = Population(populationSize, True)
+globalRoute = pop.getFittest()
+print("Initial minimum distance: " + str(globalRoute.getDistance()))
+
+# Start evolving
+fig = plt.figure()
+
+for i in pbar(range(numGenerations)):
+    pop = GA.evolvePopulation(pop)
+    localRoute = pop.getFittest()
+    if globalRoute.getDistance() > localRoute.getDistance():
+        globalRoute = localRoute
+        # globalRoute.visualization(plt)
+        # plt.draw()
+        # plt.pause(0.01)
+    yaxis.append(localRoute.getDistance())
+    xaxis.append(i)
+
+print("Count: %d Remain steps: %d" % (counter.count, numNodes * 20000 - counter.count))
+
+globalRoute.visualization(plt)
+plt.draw()
+
+print("Global minimum distance: " + str(globalRoute.getDistance()))
+print("Final Route: " + globalRoute.toString())
+
+fig = plt.figure()
+
+plt.plot(xaxis, yaxis, "r-")
+plt.show()
