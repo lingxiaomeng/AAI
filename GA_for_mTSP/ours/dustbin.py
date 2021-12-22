06238e6151d39d506bbf0b8e11687a44b8a60efa
LingxiaoMeng <574243996@qq.com>
2021-12-22 17:24:34 +0800

    v3.1

---
+++ GA_for_mTSP/ours/dustbin.py
@@ -0,0 +1,47 @@
+"""
+Represents nodes in the problem graph or network.
+Locatin coordinates can be passed while creating the object or they
+will be assigned random values.
+"""
+import random
+from . import counter
+from .routemanager import RouteManager
+from . import globals
+
+
+class Dustbin:
+    # Good old constructor
+    def __init__(self, x=None, y=None, id=-1):
+        if x == None and y == None:
+            self.x = random.randint(0, globals.xMax)
+            self.y = random.randint(0, globals.yMax)
+            self.id = -1
+        else:
+            self.x = x
+            self.y = y
+            self.id = id
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    # Returns distance to the dustbin passed as argument
+    def distanceTo(self, db):
+        dis = RouteManager.distanceMatrix[self.id][db.id]
+        counter.count = counter.count + 1
+        return dis
+
+    # Gives string representation of the Object with coordinates
+    def toString(self):
+        s = "(" + str(self.getX()) + "," + str(self.getY()) + ")"
+        return s
+
+    # Check if cordinates have been assigned or not
+    # Dusbins with (-1, -1) as coordinates are created during creation on chromosome objects
+    def checkNull(self):
+        if self.x == -1:
+            return True
+        else:
+            return False
---
+++ GA_for_mTSP/ours/globals.py
@@ -0,0 +1,15 @@
+xMax = 1000
+yMax = 1000
+seedValue = 1
+numGenerations = 100
+# size of population
+populationSize = 100
+mutationRate = 0.02
+tournamentSize = 10
+elitism = True
+numNodes = 51
+# number of trucks
+numTrucks = 5
+distance_cont = 0
+plot_progress = False
+plot_result = False
